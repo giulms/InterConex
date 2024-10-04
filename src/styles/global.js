a494850383397aab1cbd65c8f030556bf38fb5b7
victorb-s <victorb.santos15@gmail.com>
2024-10-03 21:12:46 -0300

    Adicionar estilização global
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,1 +1,24 @@
 import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*::before,
+*::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body,
+html {
+    margin: 0;
+    padding: 0;
+    max-width: 100vw;
+
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    overflow-x: hidden;
+}
+`
