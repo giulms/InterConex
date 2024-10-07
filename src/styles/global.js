438c7050961d8944db35836027da6e3a09519174
victorb-s <victorb.santos15@gmail.com>
2024-10-07 13:59:45 -0300

    Adicionar cores no style global
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- src/styles/global.js
+++ src/styles/global.js
@@ -16,9 +16,20 @@ html {
 
     box-sizing: border-box;
     scroll-behavior: smooth;
+    background-color: #ECECEC;
 
     word-wrap: break-word;
     overflow-wrap: break-word;
     overflow-x: hidden;
+}
+
+:root{
+    --corprincipal: #532567;
+    --corsecundaria: #BA68C8;
+    --corterciaria: #F4E9E1;
+
+    --varprincipal: #280F3E;
+    --varsecundaria: #9FA8D0;
+    --varterciaria: #CECBC5;
 }
 `
