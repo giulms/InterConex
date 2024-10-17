34b5832f060e08e49517dc44a44c17bfa280ae28
victorb-s <victorb.santos15@gmail.com>
2024-10-17 11:17:26 -0300

    Adicionar funcionalidade no header, barra de pesquisa na página de pesquisa
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,29 +2,34 @@ import styles from './Header.module.css'
 import LogoImage from '../../assets/logoInterConex.jpg'
 import { faBars, faBoxArchive, faUser } from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { useLocation } from 'react-router-dom'
 
 function Header() {
-    return (    
-        <header className={styles.headerContainer}>
+  const location = useLocation();
 
-        <div className={styles.headerPosition}>
-          <FontAwesomeIcon icon={faBars} className={styles.iconButton} /> 
-          <img src= {LogoImage} alt="Logo do app" className={styles.logo}/>
-        </div>
+  return (    
+      <header className={styles.headerContainer}>
 
+      <div className={styles.headerPosition}>
+        <FontAwesomeIcon icon={faBars} className={styles.iconButton} /> 
+        <img src= {LogoImage} alt="Logo do app" className={styles.logo}/>
+      </div>
+
+      {location.pathname === '/pesquisa' && (
         <div className={styles.headerPosition}>
           <form action="">
             <input className={styles.pesquisa} placeholder='Pesquise aqui...' type="text" />
             
           </form>
         </div>
+      )}
 
-        <div className={styles.headerPosition}> 
-          <FontAwesomeIcon icon={faBoxArchive} className={styles.iconButton}/>
-          <FontAwesomeIcon icon={faUser} className={styles.iconButton} />
-        </div>
-      </header>
-    )
+      <div className={styles.headerPosition}> 
+        <FontAwesomeIcon icon={faBoxArchive} className={styles.iconButton}/>
+        <FontAwesomeIcon icon={faUser} className={styles.iconButton} />
+      </div>
+    </header>
+  )
 }
 
 export default Header;
